73273c12df854e1c3acd5a02ad53404ec11da564
edwardmartinsjr <edwardmartinsjr@gmail.com>
2020-04-23 20:11:59 +0200

    add open_file

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 import sys
 import argparse
 from argparse import RawTextHelpFormatter
+import logging
+
+# create logger
+logger = logging.getLogger("channable-assignment")
+logger.setLevel(logging.DEBUG)
 
 def parse_args(args):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('path_to_before_csv', type=str)
-    parser.add_argument('path_to_after_csv', type=str)
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument("path_to_before_csv", type=str)
+        parser.add_argument("path_to_after_csv", type=str)
+        return parser.parse_args(args)
+    except SystemExit as err:
+        logger.error(f"Can\'t find all arguments: Error code [{err}]")
+        raise SystemExit()
 
-    return parser.parse_args(args)
+def open_file(path_to_open):
+    try:
+        with open(path_to_open, "r") as file:
+            return file.read()
+    except IOError as err:
+        logger.error(f"Can\'t find file or read data: {err}")
+        raise SystemExit()
 
 
-if __name__ == '__main__':
-    parse_args(sys.argv[1:])
+if __name__ == "__main__":
+
+    args = parse_args(sys.argv[1:])
+    path_to_before_csv = open_file(args.path_to_before_csv)
+    path_to_after_csv = open_file(args.path_to_after_csv)
+
--- test_main.py
+++ test_main.py
@@ -1,5 +1,8 @@
 import unittest
+from unittest.mock import patch, mock_open
 import main
+
+path_to_open = "path/to/open"
 
 # START TESTS
 class Test(unittest.TestCase):
@@ -8,5 +11,17 @@ class Test(unittest.TestCase):
         args = main.parse_args(["path_to_before_csv","path_to_after_csv"])
         self.assertEqual(args.path_to_before_csv, "path_to_before_csv")
         self.assertEqual(args.path_to_after_csv, "path_to_after_csv")
+
+    def test_parse_args_without_args(self):
+        with self.assertRaises(SystemExit): main.parse_args([])
+
+    def test_open_patch_success(self):
+        with patch("builtins.open", mock_open(read_data="data"), create=True) as m:
+            self.assertEqual(main.open_file(path_to_open), "data")
+            m.assert_called_with(path_to_open, "r")
+
+    def test_open_patch_error(self):
+        with self.assertRaises(SystemExit): main.open_file(path_to_open)
+
 
 unittest.main()
